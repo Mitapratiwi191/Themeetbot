0ba78fb2fa0563f7b66b5e98d4ed3107f5657520
Mitapratiwi191 <mirtaw40@gmail.com>
2025-06-03 14:42:50 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ownerNumber: '6282261246128', // Nomor kamu (tanpa spasi atau +)
+  botNumber: '6282191727423',   // Nomor bot (tanpa spasi atau +)
+  botName: 'Themeetbot',
+  footerText: 'Themeetbot by Mitapratiwi191',
+  prefix: '.', // Misal: .menu, .help, dll
+};
